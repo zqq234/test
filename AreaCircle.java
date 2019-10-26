63e88432860bafd36ed29e14887a92b50c1eb5fb
mayun <1544839503@qq.com>
2019-10-26 16:48:50 +0800

    根据圆的半径计算圆的面积

---
+++ AreaCircle.java
@@ -0,0 +1,14 @@
+/**
+ * @className AreaCircle
+ * @Description TODO
+ * @Author zhangqianqian
+ * @Date 2019/10/25 17:19
+ * @Version 1.0
+ **/
+public class AreaCircle {
+    public static void main(String[] args) {
+        double r=1.5;
+        double area=3.14*Math.pow(r,2);
+        System.out.println("圆的面积："+area);
+    }
+}
