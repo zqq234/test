abc4a7bdb2c01a488666a883279eb8fc86a7b1ee
mayun <1544839503@qq.com>
2019-10-31 22:41:14 +0800

    和尚打水

---
+++ FetchWater.java
@@ -0,0 +1,13 @@
+/**
+ * @className FetchWater
+ * @Description TODO
+ * @Author zhangqianqian
+ * @Date 2019/10/26 9:35
+ * @Version 1.0
+ **/
+public class FetchWater {
+    public static void main(String[] args) {
+        int count = (50 - 15) / 5;
+        System.out.println("打水次数："+count);
+    }
+}
