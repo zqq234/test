86f7fb7fae15703aa6a30dfc56b2ce232bb27245
mayun <1544839503@qq.com>
2019-11-15 14:45:46 +0800

    删除一个节点

---
+++ DeleteNode.java
@@ -0,0 +1,11 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
