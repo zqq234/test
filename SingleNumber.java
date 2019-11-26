6f1961a3decbe71071f7726359b9905e0b4cc41a
mayun <1544839503@qq.com>
2019-11-26 16:36:24 +0800

    只出现一次的数字

---
+++ SingleNumber.java
@@ -0,0 +1,9 @@
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int n=nums[0];
+        for(int i=1;i<nums.length;i++){
+            n=n^nums[i];
+        }
+        return n;
+    }
+}
