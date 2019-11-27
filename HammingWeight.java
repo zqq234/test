50d2c06dc51d6bd6f8ed5e69648e3f8705f5b2b9
mayun <1544839503@qq.com>
2019-11-27 15:55:30 +0800

    位1的个数

---
+++ HammingWeight.java
@@ -0,0 +1,11 @@
+0public class HammingWeight {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+}
