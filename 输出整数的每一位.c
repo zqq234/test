03db8801fe2a352953694c93d262d90152ed7117
mayun <1544839503@qq.com>
2019-12-10 12:57:49 +0800

    C

---
+++ 输出整数的每一位.c
@@ -0,0 +1,16 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print(int n)
+{
+	if (n>9)
+	{
+		print(n / 10);	
+	}
+	printf("%d", n % 10);
+}
+int main()
+{
+	int m=10;
+	print(m);
+	return 0;
+}
