727babd0a5ad99778df48374ae5fbd00a9c3a33e
mayun <1544839503@qq.com>
2019-12-15 23:00:47 +0800

    C

---
+++ static.c
@@ -0,0 +1,20 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int f(int a)
+{
+	int b = 0;
+	static int c = 3;
+	a = c++, b++;
+	return a;
+}
+int main()
+{
+	int a = 2, i, k;
+	for (i = 0; i < 2; i++)
+	{
+		k = f(a++);
+	}
+	printf("%d\n", k);
+
+	return 0;
+}
