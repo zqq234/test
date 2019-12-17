d7974107c4f0f542f286e355c6167e3e4a94ae51
mayun <1544839503@qq.com>
2019-12-17 15:11:15 +0800

    C

---
+++ strlen2.c
@@ -0,0 +1,13 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = (char)(-1 - i);
+	}
+	printf("%d\n", strlen(a));
+	return 0;
+}
