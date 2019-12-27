116b613a9b94856b7109a9e7bb0dd9bf1859af99
mayun <1544839503@qq.com>
2019-12-27 15:03:02 +0800

    C

---
+++ 奇数.c
@@ -0,0 +1,14 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+include<stdio.h>
+int main()
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 2 != 0)
+		{
+			printf("%d   ", i);
+		}
+	}
+	return 0;
+}
