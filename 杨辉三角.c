70730f8d9e77d9d801af8ebf6277ed8d922892a4
mayun <1544839503@qq.com>
2020-01-05 21:51:03 +0800

    C

---
+++ 杨辉三角.c
@@ -0,0 +1,27 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i, j, a[10][10];
+	for (i = 0; i < 10; i++)
+	{
+		a[i][0] = 1;
+		a[i][i] = 1;
+	}
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j < i; j++)
+		{
+			a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			printf("%-4d", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
