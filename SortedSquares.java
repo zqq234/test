c50cad7d1a4dda0ce96704ee60adeee9ac2dd980
mayun <1544839503@qq.com>
2020-01-22 14:01:26 +0800

    有序数组的平方

---
+++ SortedSquares.java
@@ -0,0 +1,9 @@
+class SortedSquares {
+    public int[] sortedSquares(int[] A) {
+        for(int i=0;i<A.length;i++){
+            A[i]=(int)Math.pow(A[i],2);
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
