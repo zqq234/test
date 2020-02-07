ad39b9ec7af8325ee883581298c137893d4bfcb7
mayun <1544839503@qq.com>
2020-02-07 11:35:49 +0800

    删除最外层的括号2

---
+++ RemoveOuterParentheses2.java
@@ -0,0 +1,12 @@
+class RemoveOuterParentheses2 {
+    public String removeOuterParentheses(String S) {
+        StringBuilder sb = new StringBuilder();
+        int level = 0;
+        for (char c : S.toCharArray()) {
+            if (c == ')') --level;
+            if (level >= 1) sb.append(c);
+            if (c == '(') ++level;
+        }
+        return sb.toString();
+    }
+}
