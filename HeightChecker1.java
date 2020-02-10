0fc25ef34dc65f5b739851b0e4b16cc9396a6341
mayun <1544839503@qq.com>
2020-02-10 21:18:53 +0800

    高度检查器1

---
+++ HeightChecker1.java
@@ -0,0 +1,16 @@
+class HeightChecker1 {
+    public int heightChecker(int[] heights) {
+        int[] arr = new int[101];
+        for (int height : heights) {
+            arr[height]++;
+        }
+        int count = 0;
+        for (int i = 1, j = 0; i < arr.length; i++) {
+            while (arr[i]-- > 0) {
+                if (heights[j++] != i) count++;
+            }
+        }
+        return count;
+    }
+}
+
