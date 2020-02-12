acb99ca7e731c9d3bf57d77a90d3acdf42147391
mayun <1544839503@qq.com>
2020-02-12 22:52:36 +0800

    链表中倒数第k个节点

---
+++ GetKthFromEnd.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class GetKthFromEnd {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(k>0){
+            fast=fast.next;
+            k--;
+        }
+        while(fast!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+}
