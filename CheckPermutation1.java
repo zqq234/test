4e5a8f04937661ed8147078a5401fe71c0bffaef
mayun <1544839503@qq.com>
2020-02-16 17:16:53 +0800

    判定是否互为字符重排

---
+++ CheckPermutation1.java
@@ -0,0 +1,11 @@
+class CheckPermutation1 {
+    public boolean checkPermutation(String s1, String s2) {
+    if(s1.length() != s2.length()) 
+        return false;
+    char[] c1 = s1.toCharArray();
+    char[] c2 = s2.toCharArray();
+    Arrays.sort(c1);
+    Arrays.sort(c2);
+    return Arrays.equals(c1, c2);
+}
+
