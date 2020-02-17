fc264655590d4ef376c4829169f98d30986f07cb
mayun <1544839503@qq.com>
2020-02-17 23:18:41 +0800

    反转整数

---
+++ ReverseInt3.java
@@ -0,0 +1,16 @@
+public int ReverseInt3(int x) {
+    public int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) 
+                return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)) 
+                return 0;
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+}
+
