973844c8eb4686eae74c7116e02acd788179ad22
mayun <1544839503@qq.com>
2020-02-17 23:17:22 +0800

    反转整数

---
+++ ReverseInt.java
@@ -0,0 +1,12 @@
+public int ReverseInt(int x) {
+	int ans = 0;
+	while (x != 0) {
+		if ((ans * 10) / 10 != ans) {
+			ans = 0;
+			break;
+		}
+		ans = ans * 10 + x % 10;
+		x = x / 10;
+	}
+	return ans;
+}
