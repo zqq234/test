efb87ab2060ade785fc97906d3c8f20fbdf0ba9d
mayun <1544839503@qq.com>
2020-02-21 14:02:13 +0800

    正则表达式匹配

---
+++ IsMatch.java
@@ -0,0 +1,15 @@
+class IsMatch {
+    public boolean isMatch(String s, String p) {
+        if(p.isEmpty()){
+            return s.isEmpty();
+        }
+        boolean flag=!s.isEmpty()&&(s.charAt(0)==p.charAt(0)||p.charAt(0)=='.');
+        if(p.length()>=2&&p.charAt(1)=='*'){
+            return isMatch(s,p.substring(2))||(flag&&isMatch(s.substring(1),p));
+        }else if(flag){
+            return isMatch(s.substring(1),p.substring(1));
+        }else{
+            return false;
+        }
+    }
+}
