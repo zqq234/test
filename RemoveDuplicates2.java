e27d54ebeff85f9a7de5b10b30ce8e4ad1fc5196
mayun <1544839503@qq.com>
2020-02-25 22:57:50 +0800

    删除排序数组中的重复项

---
+++ RemoveDuplicates2.java
@@ -0,0 +1,15 @@
+public int RemoveDuplicates2(int[] nums) {
+    if(nums == null || nums.length == 0) return 0;
+    int p = 0;
+    int q = 1;
+    while(q < nums.length){
+        if(nums[p] != nums[q]){
+            if(q - p > 1){
+                nums[p + 1] = nums[q];
+            }
+            p++;
+        }
+        q++;
+    }
+    return p + 1;
+}
