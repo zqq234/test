df90d3a6e1581cf1ba9ede9f5f1eb0cf5e5e1f96
mayun <1544839503@qq.com>
2020-02-26 23:19:28 +0800

    删除链表的倒数第N个节点

---
+++ RemoveNthFromEnd2.java
@@ -0,0 +1,20 @@
+public ListNode RemoveNthFromEnd2(ListNode head, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = dummy;
+    ListNode second = dummy;
+    for (int i = 1; i <= n + 1; i++) {
+        first = first.next;
+
+        if (first == null) {
+          return dummy.next;
+        }
+    }
+    while (first != null) {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
+}
+
