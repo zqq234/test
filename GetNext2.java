775918ac332c693eb9dda75ac3079a6209aa512c
mayun <1544839503@qq.com>
2020-03-16 12:29:05 +0800

    二叉树的下一个结点

---
+++ GetNext2.java
@@ -0,0 +1,30 @@
+/*
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
+*/
+public class GetNext2 {
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        if(pNode.right != null){
+            pNode = pNode.right;
+            while(pNode.left != null)
+                pNode = pNode.left;
+            return pNode;
+        }
+        while(pNode.next!=null){
+            if(pNode.next.left==pNode){
+                return pNode.next;
+            }
+            pNode=pNode.next;
+        }
+        return null;
+    }
+}
