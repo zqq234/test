97ae7d80513025d817f36f4039215d4dba83eb18
mayun <1544839503@qq.com>
2020-03-19 12:57:51 +0800

    二叉搜索树的最近公共祖先

---
+++ LowestCommonAncestor3.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LowestCommonAncestor3 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root!=null){
+            if(root.val>p.val&&root.val>q.val){
+                root=root.left;
+            }else if(root.val<p.val&&root.val<q.val){
+                root=root.right;
+            }else{
+                return root;
+            }
+        }
+        return root;
+    }
+}
