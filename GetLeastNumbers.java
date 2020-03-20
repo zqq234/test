e578fa70754a46565b30267189c28acf06f5d69d
mayun <1544839503@qq.com>
2020-03-20 09:04:59 +0800

    最小的k个数

---
+++ GetLeastNumbers.java
@@ -0,0 +1,10 @@
+class GetLeastNumbers {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] res=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=arr[i];
+        }
+        return res;
+    }
+}
