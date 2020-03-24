fee74e1ccb5ebe04e987bb495dbcba81ac996943
mayun <1544839503@qq.com>
2020-03-24 22:43:50 +0800

    二进制中1的个数

--- hammingWeight1.java
+++ hammingWeight1.java
@@ -1,10 +1,11 @@
-public class HammingWeight {
+public class hammingWeight {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count=0;
-        while(n!=0){
-            n=n&(n-1);
-            count++;
+        for(int i=0;i<32;i++){
+            if(((n>>i)&1)==1){
+                count++;
+            }
         }
         return count;
     }
