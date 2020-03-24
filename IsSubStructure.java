f869b3ffd3aac7822c08cd480bfac35153d9eb93
mayun <1544839503@qq.com>
2020-03-24 22:28:20 +0800

    树的子结构

---
+++ IsSubStructure.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class IsSubStructure {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+       return A!=null&&B!=null?recur(A,B)||isSubStructure(A.left,B)||isSubStructure(A.right,B):false;
+    }
+    public boolean recur(TreeNode A,TreeNode B){
+        if(B==null){
+            return true;
+        }
+        if(A==null||A.val!=B.val){
+            return false;
+        }
+        return recur(A.left,B.left)&&recur(A.right,B.right);
+    }
+}
