eaf7935d007da73d364cc6490cac5c7e09cdc290
mayun <1544839503@qq.com>
2020-03-25 15:27:44 +0800

    二进制中1的个数

---
+++ FindRepeatNumber1.java
@@ -0,0 +1,17 @@
+class FindRepeatNumber1 {
+    public int findRepeatNumber(int[] nums) {
+        int temp;
+        for(int i=0;i<nums.length;i++){
+            while (nums[i]!=i){
+                if(nums[i]==nums[nums[i]]){
+                    return nums[i];
+                }
+                temp=nums[i];
+                nums[i]=nums[temp];
+                nums[temp]=temp;
+            }
+        }
+        return -1;
+    }
+}
+
