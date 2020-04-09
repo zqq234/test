997a12c96e225fd690b4a64615bb1b0b90c38149
mayun <1544839503@qq.com>
2020-04-09 23:02:32 +0800

    删数

---
+++ DeleteNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class DeleteNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+         while (sc.hasNext()) {
+              int n = sc.nextInt();
+              if (n > 1000) {
+              n = 999;
+         }
+         List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+          }
+        int i = 0;
+       while (list.size() > 1) {
+          i = (i + 2) % list.size();
+          list.remove(i);
+        }
+        System.out.println(list.get(0));
+     }
+  }
+}
