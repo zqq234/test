11ff4cbf21feb1d91df0c195d01e1f3402ed6d3d
mayun <1544839503@qq.com>
2020-04-13 16:33:26 +0800

    无缓存交换

---
+++ Exchange1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Exchange {
+    public int[] exchangeAB(int[] AB) {
+        // write code here
+        AB[0]=AB[0]^AB[1];
+        AB[1]=AB[0]^AB[1];
+        AB[0]=AB[0]^AB[1];
+        return AB;
+    }
+}
