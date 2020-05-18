1526e71e67b62525a7960b1b608de2f1fadc7dde
mayun <1544839503@qq.com>
2020-05-18 13:41:51 +0800

    使用最小花费爬楼梯

---
+++ MinCostClimbingStairs.java
@@ -0,0 +1,12 @@
+class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<cost.length;i++){
+            int c=Math.min(cost[i]+a,cost[i]+b);
+            a=b;
+            b=c;
+        }
+        return Math.min(a,b);
+    }
+}
