e6ef453eacc9b5c2276156a3426e634e0424e665
mayun <1544839503@qq.com>
2020-05-28 09:18:30 +0800

    移除链表元素

---
+++ RemoveElements.java
@@ -0,0 +1,19 @@
+class RemoveElements {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null){
+            return null;
+        }
+        while(head!=null&&head.val==val){
+            head=head.next;
+        }
+        ListNode cur=head;
+        while(cur!=null&&cur.next!=null){
+            if(cur.next.val==val){
+                cur.next=cur.next.next;
+            }else{
+                cur=cur.next;
+            }
+        }
+        return head;
+    }
+}
