0a23de27345909e7d309e223e6d8387e0c7cb214
mayun <1544839503@qq.com>
2020-05-31 16:32:07 +0800

    环路检测

---
+++ DetectCycle1.java
@@ -0,0 +1,19 @@
+public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                fast=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
