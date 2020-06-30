95ede0a4637bf38c3f54f257cde66632eebd9948
mayun <1544839503@qq.com>
2020-06-30 22:04:39 +0800

    二叉搜索树中的搜索

---
+++ SearchBST.java
@@ -0,0 +1,14 @@
+class SearchBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root!=null){
+            if(root.val>val){
+                return searchBST(root.left,val);
+            }else if(root.val<val){
+                return searchBST(root.right,val);
+            }else{
+                return root;
+            }
+        }
+        return null;
+    }
+}
