5a126ef27c8087a0552decdf951f1947335eca1d
mayun <1544839503@qq.com>
2020-08-03 14:25:43 +0800

    数组中重复的数字

---
+++ Duplicate.java
@@ -0,0 +1,24 @@
+public class Duplicate {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        int[] arr=new int[length];
+        for(int i=0;i<length;i++){
+            if(numbers[i]<=0||numbers[i]>length-1){
+                return false;
+            }
+            arr[numbers[i]]++;
+            if(arr[numbers[i]]>1){
+                duplication[0]=numbers[i];
+                return true;
+            }
+        }
+        return false;
+    }
+}
